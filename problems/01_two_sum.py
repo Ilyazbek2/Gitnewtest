9eb93c0eaabc93d8228d3f2cc645e76685f45881
Ilyazbek2 <101692898+Ilyazbek2@users.noreply.github.com>
2026-09-23 13:47:54 +0200

    Update 01_two_sum.py

--- problems/01_two_sum.py
+++ problems/01_two_sum.py
@@ -13,7 +13,6 @@ def two_sum(numbers: list[int], target: int) -> tuple[int, int] | None:
 
     return None
 
-
 if __name__ == "__main__":
     assert two_sum([2, 7, 11, 15], 9) == (0, 1)
     assert two_sum([1, 2, 3], 10) is None
